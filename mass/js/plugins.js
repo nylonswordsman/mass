c9339865ecd40ca262b3ca00b7310be7af259c06
nylonswordsman <nylonswordsman@gmail.com>
2026-05-10 11:56:39 -0500

    culled hudmaker

--- mass/js/plugins.js
+++ mass/js/plugins.js
@@ -21,7 +21,5 @@ var $plugins =
 {"name":"r88_ProximitySensor","status":true,"description":"Calculates proximity and line of sight.","parameters":{"Obstacle Region ID":"1","Obstacle Terrain Tag":"1"}},
 {"name":"TextPicture","status":true,"description":"Displays text as a picture.","parameters":{}},
 {"name":"ButtonPicture","status":false,"description":"Makes a picture clickable.","parameters":{}},
-{"name":"McKathlin_GameOver","status":true,"description":"MZ v1.3.1 Change what happens when the party dies or Game Over is called.","parameters":{"Party Death Common Event ID":"1","Show Game Over Scene":"true","After Game Over Common Event ID":""}},
-{"name":"SRD_UltraBase","status":false,"description":"Contains code required for HUD Maker Ultra. Does not affect the RPG Maker codebase.","parameters":{}},
-{"name":"SRD_HUDMakerUltra","status":false,"description":"Allows you to create and use HUDs for the map and battles.\r\nRequires the HUD Maker Ultra editor.","parameters":{"Auto-Reload HUD Data":"false","Enable Screenshots":"false","Hide Battle Status Window":"false","Hide Battle Selection Window":"false","Fade During Events":"true","Event Fade Opacity":"125","Fade Duration":"10","Map Visibility Code":"return true;","Battle Visibility Code":"return true;"}}
+{"name":"McKathlin_GameOver","status":true,"description":"MZ v1.3.1 Change what happens when the party dies or Game Over is called.","parameters":{"Party Death Common Event ID":"1","Show Game Over Scene":"true","After Game Over Common Event ID":""}}
 ];
